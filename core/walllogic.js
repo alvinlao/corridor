6f869444b918879f77b81f3cdc555d31744f5be2
Alvin Lao <alvin.lao.is@gmail.com>
2020-05-25 15:31:02 -0700

    Clean up walllogic.js

--- core/walllogic.js
+++ core/walllogic.js
@@ -25,38 +25,36 @@ import { middle } from '../util'
 
 // isWallInbounds :: Game -> Wall -> Boolean
 // Checks whether the entire wall is on the board.
-export const isWallInbounds =
-  R.curry((game, wall) => R.all(isPointInbounds(game), points(wall)))
+export const isWallInbounds = R.curry((game, wall) =>
+  R.all(isPointInbounds(game), points(wall)))
 
 // isWallSpaceOccupied :: Game -> Wall -> Boolean
 // Checks if another wall already occupies the desired space.
-export const isWallSpaceOccupied =
-  R.curry((game, wall) => R.any(isWallOverlapping(wall), game.board.walls))
+export const isWallSpaceOccupied = R.curry((game, wall) =>
+  R.any(isWallOverlapping(wall), game.board.walls))
 
 // isWallOverlapping :: Wall -> Wall -> Boolean
 // Checks if the two walls overlap.
-export const isWallOverlapping =
-  R.curry((w1, w2) => {
-    const p1 = points(w1)
-    const p2 = points(w2)
-    return (
-      R.not(R.isEmpty(R.intersection(middle(p1), middle(p2)))) ||
-      R.length(R.intersection(p1, p2)) >= 2)
-  })
+export const isWallOverlapping = R.curry((w1, w2) => {
+  const p1 = points(w1)
+  const p2 = points(w2)
+  return R.or(
+    R.not(R.isEmpty(R.intersection(middle(p1), middle(p2)))),
+    R.length(R.intersection(p1, p2)) >= 2)
+})
 
 // isGameCompletable :: Game -> Boolean
 // Checks whether every player can reach a win location.
-export const isGameCompletable =
-  (game) => R.all(R.apply(hasPath(game)), R.toPairs(game.board.players))
+export const isGameCompletable = (game) =>
+  R.all(R.apply(hasPath(game)), R.toPairs(game.board.players))
 
 // hasPath :: Game -> PlayerId -> Player -> Boolean
 // Checks whether the player can reach a win location.
-const hasPath =
-  R.curry((game, playerId, player) => {
-    const start = playerLocation(game, playerId)
-    const stops = playerWinLocations(game, playerId)
-    return isReachable(game, start, stops, playerId)
-  })
+const hasPath = R.curry((game, playerId, player) => {
+  const start = playerLocation(game, playerId)
+  const stops = playerWinLocations(game, playerId)
+  return isReachable(game, start, stops, playerId)
+})
 
 // isReachable :: Game -> Point -> [Point] -> Boolean
 // Checks whether an unblocked path from the start point to any stop point exists.
@@ -80,24 +78,25 @@ const isReachable = R.curry((game, start, stops, playerId) => {
 
 // unvisitedNeighbours :: Game -> Point -> [Point] -> [Point]
 // Returns a list of unvisited neighbours that are on the board.
-const unvisitedNeighbours =
-  R.curry((game, point, visitedPoints) =>
-    R.reject(
-      R.anyPass([
-        R.equals(point),
-        R.complement(isPointInbounds(game)),
-        R.includes(R.__, visitedPoints),
-      ]),
-      unblockedNeighbours(game, point)))
+const unvisitedNeighbours = R.curry((game, point, visitedPoints) =>
+  R.filter(
+    R.allPass([
+      isPointInbounds(game),
+      R.complement(R.includes(R.__, visitedPoints)),
+    ]),
+    unblockedNeighbours(game, point)))
 
 // unblockedNeighbours :: Game -> Point -> [Point]
 // Returns the neighbouring points that are not blocked by a wall.
 const unblockedNeighbours = R.curry((game, point) => {
   const _unblocked = unblocked(game)
-  return R.juxt([
-    R.when(_unblocked(nedge), north),
-    R.when(_unblocked(sedge), south),
-    R.when(_unblocked(eedge), east),
-    R.when(_unblocked(wedge), west),
-  ])(point)
+  return R.pipe(
+    R.juxt([
+      R.when(_unblocked(nedge), north),
+      R.when(_unblocked(sedge), south),
+      R.when(_unblocked(eedge), east),
+      R.when(_unblocked(wedge), west),
+    ]),
+    R.reject(R.equals(point)),
+  )(point)
 })
